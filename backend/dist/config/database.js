defcc70281717de29fd7e2fe10ffcdfb8ec6172e
benjamin lopez <benjaminlopezzigaran01@gmail.com>
2026-04-27 13:05:44 -0300

    feat: configure database connection to default to PostgreSQL with environment variables

--- backend/dist/config/database.js
+++ backend/dist/config/database.js
@@ -1,30 +1,8 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const path_1 = __importDefault(require("path"));
-exports.default = ({ env }) => {
+const path = require('path');
+module.exports = ({ env }) => {
+    // Aseguramos que por defecto intente usar postgres en lugar de sqlite
     const client = env('DATABASE_CLIENT', 'postgres');
     const connections = {
-        mysql: {
-            connection: {
-                host: env('DATABASE_HOST', 'localhost'),
-                port: env.int('DATABASE_PORT', 3306),
-                database: env('DATABASE_NAME', 'strapi'),
-                user: env('DATABASE_USERNAME', 'strapi'),
-                password: env('DATABASE_PASSWORD', 'strapi'),
-                ssl: env.bool('DATABASE_SSL', false) && {
-                    key: env('DATABASE_SSL_KEY', undefined),
-                    cert: env('DATABASE_SSL_CERT', undefined),
-                    ca: env('DATABASE_SSL_CA', undefined),
-                    capath: env('DATABASE_SSL_CAPATH', undefined),
-                    cipher: env('DATABASE_SSL_CIPHER', undefined),
-                    rejectUnauthorized: env.bool('DATABASE_SSL_REJECT_UNAUTHORIZED', true),
-                },
-            },
-            pool: { min: env.int('DATABASE_POOL_MIN', 2), max: env.int('DATABASE_POOL_MAX', 10) },
-        },
         postgres: {
             connection: {
                 connectionString: env('DATABASE_URL'),
@@ -47,7 +25,7 @@ exports.default = ({ env }) => {
         },
         sqlite: {
             connection: {
-                filename: path_1.default.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+                filename: path.join(__dirname, '..', '..', env('DATABASE_FILENAME', '.tmp/data.db')),
             },
             useNullAsDefault: true,
         },
